45827ca7597b598990e4fb992d8339271df8ca38
Ahmed <ahmed1balushi3@gmail.com>
2026-08-17 23:37:38 +0400

    create an element method to create div tagsthat make up a card

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const cardOobjectDefinitions = [
     {id:3, imagePath:'/images/card-QueenDiamonds.png'},
     {id:4, imagePath:'/images/card-AceSpades.png'}
 ]
+
+function createCard(cardItem){
+    //create div elements that make up a card
+    const cardElem = createElement('div')
+    const cardInnerElem = createElement('div')
+    const cardFrontElem = createElement('div')
+    const cardBackElem = createElement('div')
+}
+
+function createElement(elemType){
+    return document.createElement('div')
+}
